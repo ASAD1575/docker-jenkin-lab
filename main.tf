68447996fc0ce281eb564be467ce87caf0570a0e
ASAD1575 <masadsubhani8@gmail.com>
2025-07-24 00:07:16 +0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -136,17 +136,22 @@ resource "aws_ecs_cluster" "node-app-cluster" {
 resource "aws_iam_role" "ecs_task_execution_role" {
   name = "ecsTaskExecutionRole"
     assume_role_policy = jsonencode({
-        Version = "2012-10-17"
-        Statement = [
+    "Version": "2012-10-17",
+    "Statement": [
         {
-            Action = "sts:AssumeRole"
-            Effect = "Allow"
-            Principal = {
-            Service = "ecs-tasks.amazonaws.com"
-            }
+            "Effect": "Allow",
+            "Action": [
+                "ecr:GetAuthorizationToken",
+                "ecr:BatchCheckLayerAvailability",
+                "ecr:GetDownloadUrlForLayer",
+                "ecr:BatchGetImage",
+                "logs:CreateLogStream",
+                "logs:PutLogEvents"
+            ],
+            "Resource": "*"
         }
-        ]
-    })
+    ]
+})
 }
 
 resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
