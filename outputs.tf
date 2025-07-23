b3c7c93244cf7669855b95ae32a5f54e3960dc8e
ASAD1575 <masadsubhani8@gmail.com>
2025-07-24 02:08:12 +0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -18,4 +18,9 @@ output "ecs_service_url" {
   value       = aws_lb.ecs_alb.dns_name
 }
 
+output "alb_dns_name" {
+    description = "DNS name of the Application Load Balancer"
+    value       = aws_lb.ecs_alb.dns_name
+  
+}
 
